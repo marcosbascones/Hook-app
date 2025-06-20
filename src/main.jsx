106eacc90cf10af319533ba5310e68164a11531c
Marcos Bascones <marcosbascones1991@gmail.com>
2025-06-20 09:07:55 +0200

    counter básico useState

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './HooksApp.jsx'
+import { CounterApp } from './01-useState/CounterApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp />
+    <CounterApp />
   </StrictMode>,
 )
