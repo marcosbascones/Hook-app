e91ba238863b0794454b20fbe64bf89ba593e318
Marcos Bascones <marcosbascones1991@gmail.com>
2025-06-20 09:25:03 +0200

    Contador múltiple que solo modifica uno de los counter

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -2,12 +2,24 @@ import React, { useState } from 'react'
 
 export const CounterApp = () => {
 
-    const [counter, setCounter] = useState(10)
+    const [{counter1, counter2, counter3}, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    })
   return (
     <>
-    <h1>Counter: {counter} </h1>
+    <h1>Counter1: {counter1} </h1>
+    <h1>Counter2: {counter2} </h1>
+    <h1>Counter3: {counter3} </h1>
     <hr />
-    <button  onClick={()=>setCounter(counter +1)}>+1</button>
+    <button  onClick={()=>setCounter({
+        counter1: counter1 + 1,
+        counter2,
+        counter3
+        //Es lo mismo que:
+        // counter3: counter3
+    })}>+1</button>
     
 
     </>
