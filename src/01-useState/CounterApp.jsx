95392e0bd98791110c60ee9c912a06d57fa51740
Marcos Bascones <marcosbascones1991@gmail.com>
2025-06-20 09:28:11 +0200

    Se añade operador spread

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 
 export const CounterApp = () => {
 
-    const [{counter1, counter2, counter3}, setCounter] = useState({
+    const [state, setCounter] = useState({
         counter1: 10,
         counter2: 20,
         counter3: 30,
     })
+
+    const { counter1, counter2, counter3 } = state;
   return (
     <>
     <h1>Counter1: {counter1} </h1>
@@ -14,9 +16,10 @@ export const CounterApp = () => {
     <h1>Counter3: {counter3} </h1>
     <hr />
     <button  onClick={()=>setCounter({
+        ...state, // Uso de spread Mantiene el resto de las propiedades del objeto state 
         counter1: counter1 + 1,
-        counter2,
-        counter3
+        
+        //counter3
         //Es lo mismo que:
         // counter3: counter3
     })}>+1</button>
