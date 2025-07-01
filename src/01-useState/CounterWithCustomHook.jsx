4dae93338065248f68c7b363323a5afc9f107fb8
Marcos Bascones <marcosbascones1991@gmail.com>
2025-07-01 13:13:28 +0200

    Inicializacion de CounterWithCustom

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const CounterWithCustomHook = () => {
+  return (
+    <>
+        <h1>Counter with Hook: xxx</h1>
+        <hr />
+        <button className='btn btn-primary'>+1</button>
+        <button className='btn btn-primary'>-1</button>
+        <button className='btn btn-primary'>Reset</button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './HooksApp.jsx'
 import { CounterApp } from './01-useState/CounterApp.jsx'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </StrictMode>,
 )
