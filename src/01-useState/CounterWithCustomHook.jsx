fa131f489ad510c60f88cf8537bc3027749547d4
Marcos Bascones <marcosbascones1991@gmail.com>
2025-07-02 10:00:22 +0200

    Se incorpora JS useCounter en carpeta nueva hooks de src

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useCounter } from '../hooks/useCounter'
 
 export const CounterWithCustomHook = () => {
+
+  const {counter}= useCounter();
+
+
   return (
     <>
-        <h1>Counter with Hook: xxx</h1>
+        <h1>Counter with Hook: {counter}</h1>
         <hr />
         <button className='btn btn-primary'>+1</button>
         <button className='btn btn-primary'>-1</button>
