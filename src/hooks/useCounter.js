df3c65cbd8ee4e1df8d0748b1cfaecb29fff9b50
Marcos Bascones <marcosbascones1991@gmail.com>
2025-07-02 11:13:57 +0200

    Implementacion de CounterWithCustomHook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -4,7 +4,20 @@ export const useCounter = (initialValue=10) => {
 
     const [counter, setCounter] = useState(initialValue)
 
+    const increment = (value =1) => {
+        setCounter(counter + value);
+    }
+    const decrement = (value=1) => {
+        if (counter === 0) return; // Prevents decrementing below 0
+        setCounter(counter - value);
+    }
+    const reset = () => {
+        setCounter(initialValue);
+    }
+
+
+
     return {
-        counter
+        counter, increment, decrement, reset
     }
 }
